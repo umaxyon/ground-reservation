7508ca0f07c91df1b0ea8d5a43e7f712ac6a697e
Umaxyon <umaxyon@gmail.com>
2021-03-05 15:38:33 +0900

    modelにMaxLengthValidator導入

--- webapi/ground_view/models.py
+++ webapi/ground_view/models.py
@@ -1,13 +1,14 @@
 from django.db import models
+from django.core.validators import MaxLengthValidator
 
 
 class GroundInfo(models.Model):
-    ym = models.IntegerField('年月', max_length=6)
-    dt = models.IntegerField('日', max_length=2)
+    ym = models.IntegerField('年月', validators=[MaxLengthValidator(6)])
+    dt = models.IntegerField('日', validators=[MaxLengthValidator(2)])
     week_day = models.CharField('曜日', max_length=1)
     area = models.CharField('地域', max_length=10)
     gname = models.CharField('グラウンド名', max_length=15)
-    timebox = models.IntegerField('時間帯', max_length=2)
+    timebox = models.IntegerField('時間帯', validators=[MaxLengthValidator(2)])
 
     def __str__(self):
         return f"[{self.ym}{self.dt:0>2}_{self.timebox}]{self.gname}"
