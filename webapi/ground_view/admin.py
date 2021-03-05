d7c14d3f4bf9857b34092bc6ea734eab700cbb21
Umaxyon <umaxyon@gmail.com>
2021-03-05 15:18:12 +0900

    django導入。webapiアプリ追加。ground_viewプロジェクト作成。

---
+++ webapi/ground_view/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from ground_view.models import GroundInfo
+
+admin.site.register(GroundInfo)
---
+++ webapi/ground_view/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class GroundViewConfig(AppConfig):
+    name = 'ground_view'
