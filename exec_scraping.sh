26ebde93c449f23bf247044f210b06ad6333ff59
Umaxyon <umaxyon@gmail.com>
2021-03-08 02:29:42 +0900

    scraping起動シェルひな形修正

--- exec_scraping.sh
+++ exec_scraping.sh
@@ -4,7 +4,9 @@ cd "$(dirname "$0")" || exit 1
 source venv/bin/activate
 
 # ユーザーとパスを含めてpushしないこと!
+export DB_HOST=localhost
+export DB_PORT=
 export DB_USER=
 export DB_PASS=
 
-python scraping/Scraper.py
+python scraping/scheduler.py
