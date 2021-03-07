6a434a6690dc37f45a91be06d5cf8204d4427b29
Umaxyon <umaxyon@gmail.com>
2021-03-07 14:57:05 +0900

    Daoのtransactionをデコレータ化

--- scraping/Dao.py
+++ scraping/Dao.py
@@ -1,5 +1,26 @@
 import os
 import MySQLdb
+from sqlalchemy.pool import QueuePool
+
+
+def transaction(f):
+    def wrapped(inst, *args, **kwargs):
+        inst.conn = inst.pool.connect()
+        inst.cur = inst.conn.cursor()
+        try:
+            ret = f(inst, *args, **kwargs)
+            inst.conn.commit()
+            return ret
+        except Exception as e:
+            inst.conn.rollback()
+            raise e
+        finally:
+            inst.cur.close()
+            inst.conn.close()
+            inst.cur = None
+            inst.conn = None
+
+    return wrapped
 
 
 class Dao:
@@ -12,21 +33,20 @@ class Dao:
             "db": "ground_reservation",
             "charset": "utf8mb4"
         }
+        self.pool = QueuePool(lambda :MySQLdb.connect(**self.conf), pool_size=3)
+        self.conn = None
+        self.cur = None
 
+    @transaction
+    def get_system_condition(self):
+        self.cur.execute('select * from ground_view_systemcondition WHERE id = %s', [1])
+        return self.cur.fetchone()
+
+    @transaction
     def recreate_groundinfo(self, params):
-        conn = MySQLdb.connect(**self.conf)
-        cur = conn.cursor()
-        try:
-            cur.execute("delete from ground_view_groundinfo")
-            cur.executemany((
-                "insert into ground_view_groundinfo(ym, dt, week_day, area, gname, timebox) "
-                "values(%s, %s, %s, %s, %s, %s)"
-            ), params)
-            conn.commit()
-        except Exception as e:
-            conn.rollback()
-            raise e
-        finally:
-            cur.close()
-            conn.close()
+        self.cur.execute("delete from ground_view_groundinfo")
+        self.cur.executemany((
+            "insert into ground_view_groundinfo(ym, dt, week_day, area, gname, timebox) "
+            "values(%s, %s, %s, %s, %s, %s)"
+        ), params)
 
