6692233b6bc9f38e5782ef87ebac87519018f8c8
Umaxyon <umaxyon@gmail.com>
2021-03-09 15:12:22 +0900

    RservationPlan、ReservationTargetをモデル追加。migration更新。

--- webapi/ground_view/admin.py
+++ webapi/ground_view/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from ground_view.models import GroundInfo, SystemCondition
+from ground_view.models import GroundInfo, SystemCondition, ReservationPlan, ReservationTarget
 
 
 class GraundInfoAdmin(admin.ModelAdmin):
@@ -10,5 +10,15 @@ class SystemConditionAdmin(admin.ModelAdmin):
     list_display = ('available', 'debug', 'last_update')
 
 
+class ReservationPlanAdmin(admin.ModelAdmin):
+    list_display = ('status', 'ymd_range', 'area_csv')
+
+
+class ReservationTargetAdmin(admin.ModelAdmin):
+    list_display = ('ym', 'dt', 'week_day', 'area', 'gname', 'timebox')
+
+
 admin.site.register(GroundInfo, GraundInfoAdmin)
 admin.site.register(SystemCondition, SystemConditionAdmin)
+admin.site.register(ReservationPlan, ReservationPlanAdmin)
+admin.site.register(ReservationTarget, ReservationTargetAdmin)
--- webapi/ground_view/models.py
+++ webapi/ground_view/models.py
@@ -21,3 +21,23 @@ class SystemCondition(models.Model):
     available = models.IntegerField('システム利用可否', validators=[reg_on_off])
     debug = models.IntegerField('デバッグモード', validators=[reg_on_off])
     last_update = models.CharField('最終更新', max_length=19, validators=[reg_ymdhms])
+
+
+class ReservationPlan(models.Model):
+    status = models.CharField('状態', max_length=10)
+    ymd_range = models.CharField('年月日範囲', max_length=17, default='')
+    area_csv = models.CharField('地域', max_length=80, default='')
+
+
+class ReservationTarget(models.Model):
+    plan = models.ForeignKey(ReservationPlan, on_delete=models.CASCADE, null=True)
+    status = models.CharField('状態', max_length=10)
+
+    gno = models.IntegerField('号面', validators=[MaxLengthValidator(2)], default=0)
+    ym = models.IntegerField('年月', validators=[MaxLengthValidator(6)])
+    dt = models.IntegerField('日', validators=[MaxLengthValidator(2)])
+    week_day = models.CharField('曜日', max_length=1)
+    area = models.CharField('地域', max_length=10)
+    gname = models.CharField('グラウンド名', max_length=15)
+    gno_csv = models.CharField('号面CSV', max_length=15, default='')
+    timebox = models.IntegerField('時間帯', validators=[MaxLengthValidator(2)])
