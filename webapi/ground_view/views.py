dc3e7d2b28a5b9f7db2610d82636be5b981ad6ea
Umaxyon <umaxyon@gmail.com>
2021-03-11 00:26:57 +0900

    get_plansサービス追加

--- webapi/ground_view/views.py
+++ webapi/ground_view/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http.response import JsonResponse
 from django.views.decorators.csrf import ensure_csrf_cookie
-from ground_view.models import SystemCondition
+from ground_view.models import SystemCondition, ReservationPlan
+from itertools import groupby
 
 # Create your views here.
 
@@ -19,3 +20,19 @@ def get_system_condition(req):
         "debug": cond.debug == 1,
         "last_update": cond.last_update
     })
+
+
+@ensure_csrf_cookie
+def get_plans(req):
+    items = ReservationPlan.objects.all().order_by('status').reverse()
+    ret = {}
+    for k, plans in groupby(list(items), key=lambda p: p.status):
+        ret[k] = sorted([{
+                'id': p.id,
+                'ymd_range': p.ymd_range,
+                'status': p.status,
+                'area_csv': p.area_csv
+            } for p in plans],
+            key=lambda p: p['ymd_range'], reverse=True)
+
+    return JsonResponse(ret)
