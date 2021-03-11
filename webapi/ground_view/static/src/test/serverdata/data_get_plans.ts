8759bf7241d3e3bcb644a9203e5e811e68ffce13
Umaxyon <umaxyon@gmail.com>
2021-03-12 03:01:33 +0900

    Webローカルテストデータ追加。

--- webapi/ground_view/static/src/test/serverdata/data_get_plans.ts
+++ webapi/ground_view/static/src/test/serverdata/data_get_plans.ts
@@ -1,8 +1,12 @@
 export default (param: any) => {
     return {
         '監視中': [
-            {'id': 1, 'ymd_range': '20210313-20210313', 'status': '監視中', 'area_csv': '蒲田'},
-            {'id': 2, 'ymd_range': '20210318-20210319', 'status': '監視中', 'area_csv': '蒲田,大森'} 
+            {'id': 1, 'ymd_range': '20210313-20210313', 'author': 'user',
+                'status': '監視中', 'area_csv': '蒲田', 'target_cnt': 5, 'reserved_cnt': 3},
+            {'id': 2, 'ymd_range': '20210318-20210319', 'author': 'auto',
+                'status': '監視中', 'area_csv': '蒲田,大森', 'target_cnt': 10, 'reserved_cnt': 0},
+            {'id': 3, 'ymd_range': '20210320-20210321', 'author': 'auto',
+                'status': '監視中', 'area_csv': '蒲田,大森', 'target_cnt': 8, 'reserved_cnt': 0} 
         ]
     }
 }
