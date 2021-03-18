7330dc4073ddbd2e44ecdaff4321b7aee64c329a
Umaxyon <umaxyon@gmail.com>
2021-03-18 16:57:22 +0900

    プランの日付固定に伴うテストデータ修正

--- webapi/ground_view/static/src/test/serverdata/data_get_plans.ts
+++ webapi/ground_view/static/src/test/serverdata/data_get_plans.ts
@@ -2,11 +2,11 @@ export default (param: any) => {
     return {
         'plans':     {
             '監視中': [
-                {'id': 1, 'ymd_range': '20210313-20210313', 'author': 'user',
+                {'id': 1, 'ymd_range': '20210313', 'author': 'user',
                     'status': '監視中', 'area_csv': '蒲田', 'target_cnt': 5, 'reserved_cnt': 3},
-                {'id': 2, 'ymd_range': '20210318-20210319', 'author': 'auto',
+                {'id': 2, 'ymd_range': '20210318', 'author': 'auto',
                     'status': '監視中', 'area_csv': '蒲田,大森', 'target_cnt': 10, 'reserved_cnt': 0},
-                {'id': 3, 'ymd_range': '20210320-20210321', 'author': 'auto',
+                {'id': 3, 'ymd_range': '20210320', 'author': 'auto',
                     'status': '監視中', 'area_csv': '蒲田,大森', 'target_cnt': 8, 'reserved_cnt': 0} 
             ]
         },
